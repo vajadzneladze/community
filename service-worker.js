d069d10bd55521cadc8f191abb8cd1d28dbc93cd
vaja dzneladze <vaja.dznealdze@gmail.com>
2019-04-20 21:45:55 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/community/precache-manifest.ca244346eae72dc84896432a84004a1c.js"
+  "/community/precache-manifest.61bbf5ca98999d0e7edcf63a3ae8140f.js"
 );
 
 workbox.clientsClaim();
