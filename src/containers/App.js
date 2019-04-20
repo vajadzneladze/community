ed15658de94ef6b3f275ac6ce0b0df6064828b26
vaja dzneladze <vaja.dznealdze@gmail.com>
2019-04-20 21:26:10 +0400

    retrive Subjects from TsuCommunity Api for test

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+  	subjects : []
+  }
+
+
+  componentDidMount(){
+
+  	const url = 'http://tsu.creativestudio.ge/subjects';
+
+  	setInterval(()=> {
+  		this.getSubjects(url);
+  	}, 100)
+
+  	console.log('componentDidMount', this.state.subjects);
+  	 
+  }
+
+  	getSubjects = async url => {
+
+	    axios.get(url).then( res => {
+	    	this.setState({
+	    		subjects:res.data
+	    	});
+	    });
+
+	};
+
+
+
+
   render() {
+
     return (
-      <div className="App">
-       
+      <div className="App"> 
+       		<table border="1">
+				  <thead>
+					  <tr>
+					    <th>ID</th>
+					    <th>title</th>
+					    <th>faculty ID</th>
+					   
+					  </tr>
+				  </thead>
+				  <tbody>
+				  {
+				  	
+				  	this.state.subjects.map( item => {
+				  		
+				  		return (
+				  			<tr key =  { item.id } >
+					  			<td> { item.id } </td>
+					  			<td> { item.title } </td>
+					  			<td> { item.faculty_id } </td>
+				  			</tr>
+				  		)
+
+				  	})
+
+				  }
+		  		</tbody>
+  			 </table>
       </div>
     );
   }
