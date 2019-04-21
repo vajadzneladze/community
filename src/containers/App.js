d2804e27871e9f20fd7f484c1e6cc442df0fa9b5
vaja dzneladze <vaja.dznealdze@gmail.com>
2019-04-21 21:50:01 +0400

    delete favicon from package json minify

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
 import Auth from './Auth/Auth';
 
 class App extends Component {
-
-  state = {
-  	subjects : []
-  }
-
-
-  componentDidMount(){
-		const url = 'http://tsu.creativestudio.ge/subjects';
-		this.getSubjects(url);
-  }
-
-	getSubjects = async url => {
-
-		axios.get(url).then( res => {
-			this.setState({ 
-				subjects:res.data
-			})
-		})
-	}
 
   render(){
     return (
